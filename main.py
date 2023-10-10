1098e9db5d9647eb511aa9ba828ffaab64b13b8e
Sai Pavan Kumar Meruga <saipavan.meruga@gmail.com>
2023-10-10 14:54:43 -0400

    chore: stage 03 data transformation code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationStagePipeline
 from textSummarizer.logging import logger
 
@@ -21,4 +22,12 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
-
+STAGE_NAME="Data Transformation Stage"
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
